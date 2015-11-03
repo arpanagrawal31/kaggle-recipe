94cbb57e3726c723e7cee38b8b89125fb282eb4e
anirudt <anirudt@gmail.com>
2015-11-03 21:28:10 +0530

    obtained 6714 count, deprecated Counter

--- temp.py
+++ temp.py
@@ -9,7 +9,6 @@ import json
 
 with open("train.json") as json_file:
     json_data = json.load(json_file)
-    print(json_data)
 
 dol = []
 for i in json_data:
@@ -19,21 +18,22 @@ dol = list(set(dol))
 ing = []
 for j in json_data:
     ing.append(j["ingredients"])
-ing = list(set(ing))
 
 alling = []
 for k in ing:
     for l in k:
         alling.append(l)
         
+alling = list(set(alling))
+
 str_alling = []
 str_alling = [x.replace(' ','') for x in alling]
 
-from collections import Counter
-counts = Counter(str_alling)
-print(counts)
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#vect = TfidfVectorizer(input = str_alling, ngram_range = (1, 1), analyzer = 'word', min_df = 1)
-#y = vect.fit_transform(str_alling)
-#feature_names = vect.get_feature_names()
-
+#from collections import Counter
+#counts = Counter(str_alling)
+#print(counts)
+from sklearn.feature_extraction.text import TfidfVectorizer
+vect = TfidfVectorizer(input = str_alling, ngram_range = (1, 1), analyzer = 'word', min_df = 1)
+y = vect.fit_transform(str_alling)
+feature_names = vect.get_feature_names()
+print y
